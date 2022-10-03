dc295f1a7ec3af889791c1c49e0994b110ab4642
FranDiana <din3711din@gmail.com>
2022-10-03 16:21:19 +0300

    fds

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] a = {1, 2, 3, 3, 9, 1};
+        boolean isPolindrom = true;
+        int i;
+        for (i = 0; i < a.length; i++) {
+            if (a[i] != a[a.length - i - 1]) {
+                isPolindrom = false;
+                break;
+            }
+        }
+        if (isPolindrom == false) {
+            System.out.println("it`s not a polindrom");
+        } else System.out.println("it`s a polindrom");
     }
 }
